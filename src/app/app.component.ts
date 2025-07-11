ebf81e554c8dfa86f3842e1051d8db0288e0cb0e
Yury <yury.katko@walgreens.com>
2025-07-11 08:45:16 -0400

    localize title too

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    standalone: false
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  standalone: false
 })
-export class AppComponent {
-  title = 'lunch-plans';
+export class AppComponent implements OnInit {
+  constructor(private titleService: Title) { }
+
+  ngOnInit() {
+    this.titleService.setTitle($localize`Lunch Plans`);
+  }
 }
